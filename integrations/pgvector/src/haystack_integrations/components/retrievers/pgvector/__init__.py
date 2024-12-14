27beaf15d4bdac4462ed8955481ee86029f48be8
Adebayo Oshingbesan <dehbaiyor@gmail.com>
2024-12-14 18:19:10 +0000

    fix: import

--- integrations/pgvector/src/haystack_integrations/components/retrievers/pgvector/__init__.py
+++ integrations/pgvector/src/haystack_integrations/components/retrievers/pgvector/__init__.py
@@ -3,5 +3,6 @@
 # SPDX-License-Identifier: Apache-2.0
 from .embedding_retriever import PgvectorEmbeddingRetriever
 from .keyword_retriever import PgvectorKeywordRetriever
+from .hybrid_retriever import PgvectorHybridRetriever
 
-__all__ = ["PgvectorEmbeddingRetriever", "PgvectorKeywordRetriever"]
+__all__ = ["PgvectorEmbeddingRetriever", "PgvectorKeywordRetriever", "PgvectorHybridRetriever"]
